7ca1750307c9a99e73b256550af2a7e61ebdf9e5
ALDLions-Chess <aldershotchessclub@gmail.com>
2026-01-16 05:32:59 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = 'ald-chess_club-homepage'; // Change if your repo name is different
+const repoName = 'ald-chess_club-homepage'; //  Change if your repo name is different
 
 const nextConfig = {
   output: 'export',
